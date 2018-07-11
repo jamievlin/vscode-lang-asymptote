28a40e026d04a19ebf3df7fb7c13ce2e0a617c89
Supakorn Rassameemasmuang <supakorn.ras@outlook.com>
2018-07-11 13:47:31 -0600

    Handle reading in debug messages.

--- asydbg/asydbg_.py
+++ asydbg/asydbg_.py
@@ -1,10 +1,27 @@
 
+#!/usr/bin/env python3
 
 import sys
 import io
+import json
+
+def read_msg() -> dict:
+    header = sys.stdin.readline()
+    assert header.startswith('Content-Length: ')
+    bytenum = int(header.replace('Content-Length: ', '', 1))
+    sys.stdin.readline()
+
+    msg = sys.stdin.read(bytenum)
+    return json.loads(msg)
+
+def log(txt: str):
+    sys.stderr.write(txt)
+    sys.stderr.write('\n')
+    # sys.stderr.flush()
 
 def main(args):
-    sys.stderr.write(sys.stdin.readline())
+    msg = read_msg()
+    log(str(msg))
 
 
 if __name__ == '__main__':
